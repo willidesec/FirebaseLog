a740801ecdfbe5da3246bae6e9712117acce3c53
William <william.desecot@hotmail.fr>
2019-05-01 18:07:29 +0200

    Initial Commit

---
+++ FirebaseLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseLog
+//
+//  Created by William on 01/05/2019.
+//  Copyright © 2019 William Désécot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
